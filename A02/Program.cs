32ded9f5ee0ba5c6c21d4ab820c0ccfab533ba09
stefyrose <AmalStefy.Regis@trumpf.com>
2025-11-04 15:27:04 +0530

    A02: Corrected the prompt

--- A02/Program.cs
+++ A02/Program.cs
@@ -1,8 +1,15 @@
-﻿using static System.Console;
+﻿// ------------------------------------------------------------------------------------------------
+// Training ~ A training program for new joinees at Metamation, Batch- July 2025.
+// Copyright (c) Metamation India.
+// ------------------------------------------------------------------
+// Program.cs
+// Program prompts the user to guess the system generated number between 1 to 100.
+// ------------------------------------------------------------------------------------------------
+using static System.Console;
 
 int num = new Random ().Next (1, 101);
 for (; ; ) {
-   Write ("Guess a Number between 1 to 100: ");
+   Write ("Guess a number between 1 to 100: ");
    if (int.TryParse (ReadLine (), out int guess)) {
       if (num == guess) {
          WriteLine ("You got it right!"); break;
